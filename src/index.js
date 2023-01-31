f07d23d6f88400a9bf1fb83e58f3626f9ea8f998
unknown <Gejjeg>
2023-01-31 21:52:31 +0100

    feat: Components - 04 added

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import { createRoot } from "react-dom/client"
 import { Hello } from "./helloComponent";
 import { App } from "./appComponent";
+import * as ReactDOM from 'react-dom';
+
 
 const root = createRoot(document.getElementById("root"));
 
 root.render(<Hello />);
 root.render(<App />);
+ReactDOM.render(<App />, document.getElementById("root"));
