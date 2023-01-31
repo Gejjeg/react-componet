b525a90432966187d6e1feebe5b148f1ed4f424a
unknown <Gejjeg>
2023-01-31 20:42:16 +0100

    feat: Components 03 added

---
+++ src/appComponent.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Hello } from "./helloComponent";
+
+export class App extends React.Component {
+    render() {
+        <div>{Hello}</div>;
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import { createRoot } from "react-dom/client"
 import { Hello } from "./helloComponent";
+import { App } from "./appComponent";
 
 const root = createRoot(document.getElementById("root"));
 
 root.render(<Hello />);
+root.render(<App />);
 
