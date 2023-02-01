141c29fb8f847e25cb4f75c4a3386c621d342ea8
unknown <Gejjeg>
2023-02-01 05:37:30 +0100

    feat: Components - 06 added

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import { createRoot } from "react-dom/client"
 import { Hello } from "./helloComponent";
 import { App } from "./appComponent";
 import * as ReactDOM from 'react-dom';
+import { ModifyHello } from "./modifyHello";
 
 
 const root = createRoot(document.getElementById("root"));
@@ -9,3 +10,4 @@ const root = createRoot(document.getElementById("root"));
 root.render(<Hello />);
 root.render(<App />);
 ReactDOM.render(<App />, document.getElementById("root"));
+root.render(<ModifyHello />);
